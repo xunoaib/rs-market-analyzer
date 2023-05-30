7311f31a8f6f4616a4728e4f3efcc4160fbbdb61
naddiemathkour <naddiemathkour@gmail.com>
2023-05-29 20:14:30 -0400

    Graphing added to naddie.py

--- rsmarket/examples/naddie.py
+++ rsmarket/examples/naddie.py
@@ -9,31 +9,47 @@ from pathlib import Path
 
 import rsmarket
 from dotenv import load_dotenv
-from rsmarket.dbschema import ItemInfo, LatestPrice
+from rsmarket.dbschema import ItemInfo, AvgHourPrice
 from sqlalchemy import create_engine, false, func, select
 from sqlalchemy.orm import Session
 from tabulate import tabulate
+from matplotlib import pyplot as plt
+import seaborn as sb
+import pandas as pd
 
 
 def demo(session: Session):
 
     # find the latest timestamp so we can ignore older logs
-    latest_time = select(func.max(LatestPrice.timestamp)).scalar_subquery()
+    latest_time = select(func.max(AvgHourPrice.timestamp)).scalar_subquery()
 
     # construct query
+    profit = ((AvgHourPrice.avgHighPrice-AvgHourPrice.avgLowPrice)*ItemInfo.limit).label('profit')
+    weekly = AvgHourPrice.timestamp >= latest_time - 600000
+
     query = (
-        select(ItemInfo.name, ItemInfo.limit, ItemInfo.value, LatestPrice.high, LatestPrice.low)  # select columns to show
-        .join(LatestPrice, LatestPrice.id == ItemInfo.id)  # join with latest item prices
+        select(ItemInfo.name, ItemInfo.limit, AvgHourPrice.avgHighPrice, AvgHourPrice.avgLowPrice, AvgHourPrice.timestamp, profit)  # select columns to show
+        .join(AvgHourPrice, AvgHourPrice.id == ItemInfo.id)  # join with latest item prices
+        .where(ItemInfo.name == 'Yellow bead')
         .where(ItemInfo.members == false())  # only list F2P items
         .where(ItemInfo.name != 'Old school bond') # exclude bonds from query
-        .where(LatestPrice.timestamp >= latest_time - 5000)  # only show the latest price
-        .order_by(ItemInfo.name)  # in decreasing order of price
-        .limit(100)  # limit to 10 rows
+        .where(AvgHourPrice.timestamp >= latest_time - 500000)  # only show the latest price
+        #.limit(1000)  # limit to 10 rows
     )
 
     # execute query
     result = session.execute(query)
     rows = result.all()
+
+
+    dataframe = pd.read_sql_query(query, create_engine(os.environ['DB_ENGINE_URL'], echo=False))
+    print(dataframe.columns)
+    ax = sb.lineplot(x='timestamp', y='profit', data=dataframe, hue='name', palette='colorblind', legend=False)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
+    for i in ax.containers:
+        ax.bar_label(i,)
+    plt.tight_layout()
+    plt.show()
 
     # print nice tabular output with headers and timestamps
     headers = list(result.keys())
