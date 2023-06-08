efd2d9091c81002f2003cd5be7c859c55d2992f6
xunoaib <xunoaib@gmail.com>
2023-06-08 05:11:28 -0400

    Fix percentiles and add arguments

--- rsmarket/examples/percentiles.py
+++ rsmarket/examples/percentiles.py
@@ -1,57 +1,72 @@
 #!/usr/bin/env python3
+import argparse
 import pickle
 
 import pandas as pd
+from rsmarket.dbschema import AvgFiveMinPrice, ItemInfo
 from rsmarket.main import get_engine
-from rsmarket.dbschema import ItemInfo, LatestPrice, AvgHourPrice, AvgFiveMinPrice
-from sqlalchemy import create_engine, false, func, select
+from sqlalchemy import func, select
+
+CACHE_FILE = 'cache.pickle'
 
 
-def df_query_with_cache(query, engine):
+def write_cache(query, df, fname=CACHE_FILE):
+    with open(fname, 'wb') as f:
+        pickle.dump((df, str(query)), f)
+
+
+def read_cache(fname):
+    with open(fname, 'rb') as f:
+        return pickle.load(f)
+
+
+def read_cache_or_request(query, engine, fname=CACHE_FILE):
     '''
-    Returns a cached query result or executes it if it doesn't exist or has changed.
-    Note: Parameterized args/values aren't cached, so manual deletion
-    of the cache may be necessary to ensure accurate results.
+    Tries to load a cached dataframe, but will execute the query and re-cache
+    if the query structure has changed or the cache file doesn't exist.
     '''
     try:
-        with open('cache.query.sql') as f:
-            if str(query).strip() != f.read().strip():
-                raise FileNotFoundError
-        with open('cache.pickle', 'rb') as f:
-            return pickle.load(f)
+        df, cached_query = read_cache(fname)
+        if str(query) != cached_query:
+            raise FileNotFoundError
     except FileNotFoundError:
-        print('Executing fresh query')
         df = pd.read_sql_query(query, engine)
-        with open('cache.pickle', 'wb') as f:
-            pickle.dump(df, f)
-        with open('cache.query.sql', 'w') as f:
-            f.write(str(query))
-        return df
+        write_cache(query, df, fname)
+    return df
 
 
 def main():
-    engine = get_engine()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-c',
+        '--cache',
+        action='store_true',
+        help='Load query result from cache'
+    )
+    parser.add_argument('item', help='Item name to search for')
+    args = parser.parse_args()
 
+    engine = get_engine()
     latest_time = select(func.max(AvgFiveMinPrice.timestamp)).scalar_subquery()
     query = (
-        select(ItemInfo.name, AvgFiveMinPrice, ItemInfo.limit)
-        .join(AvgFiveMinPrice, AvgFiveMinPrice.id == ItemInfo.id)
-        # .where(ItemInfo.members == false())
-        .where(ItemInfo.name == 'Uncut sapphire')
-        .where(AvgFiveMinPrice.timestamp > latest_time - 60 * 60 * 24 * 1)
-        .order_by(ItemInfo.value.desc())
+        select(ItemInfo.name, AvgFiveMinPrice, ItemInfo.limit)  #
+        .join(AvgFiveMinPrice, AvgFiveMinPrice.id == ItemInfo.id)  #
+        .where(func.lower(ItemInfo.name) == func.lower(args.item))  #
+        .where(AvgFiveMinPrice.timestamp > latest_time - 60 * 60 * 24 * 1)  #
+        .order_by(ItemInfo.value.desc())  #
     )
 
-    df = df_query_with_cache(query, engine)  # cached
-    # df = pd.read_sql_query(query, engine)  # non-cached
+    if args.cache:
+        df = read_cache_or_request(query, engine)
+    else:
+        df = pd.read_sql_query(query, engine)
+        write_cache(query, df)
 
-    COL_PAIRS = [
-        ('avgHighPrice', 'highPriceVolume'),
-        ('avgLowPrice', 'lowPriceVolume')
-    ]
-
+    PRICE_COLS = ('avgHighPrice', 'avgLowPrice')
+    VOLUME_COLS = ('highPriceVolume', 'lowPriceVolume')
     dfs = []
-    for priceCol, volumeCol in COL_PAIRS:
+
+    for priceCol, volumeCol in zip(PRICE_COLS, VOLUME_COLS):
         dft = df.drop(list(set(df.columns) - {priceCol, volumeCol}), axis=1) \
                 .rename(columns={priceCol: 'price', volumeCol: 'volume'}) \
                 .groupby(['price']).sum().reset_index() \
@@ -60,16 +75,20 @@ def main():
         dfs.append(dft)
 
     if not all(len(d) for d in dfs):
-        print('Error: Empty result')
+        print('Error: no results found')
         return
 
-    percs = [0, 5, 10, 25]  # top, 5th, 10th, and 25th percentiles
+    percentiles = [0, 5, 10, 25]  # top, 5th, 10th, and 25th percentiles
 
-    for dft in dfs:
-        for perc in percs:
-            row = dft.loc[(dft.percentile >= (100 - perc) / 100).idxmax()]
-            print(f'{perc:>3}th percentile =', int(row['price']), 'gp')
-        print('-' * 28)
+    print(' Price percentiles for "{}"\n'.format(df['name'][0]))
+    for dft, label in zip(dfs, ['Instabought', 'Instasold']):
+        print(f' {label} percentiles')
+        print(' ' + '-' * 25)
+        for percentile in percentiles:
+            row = dft.loc[(dft.percentile >= percentile / 100).idxmax()]
+            print(f'{percentile:>3}th percentile =', int(row['price']), 'gp')
+        print()
+
 
 if __name__ == '__main__':
     main()
